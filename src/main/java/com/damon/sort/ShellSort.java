90ca8e898c24ff113239cb67a59aaa11be106768
damon0425 <damon0425@outlook.com>
2020-07-07 22:31:26 +0800

    1.shell算法的完成

--- src/main/java/com/damon/sort/ShellSort.java
+++ src/main/java/com/damon/sort/ShellSort.java
@@ -11,6 +11,36 @@ package com.damon.sort;
 public class ShellSort extends InsertSort{
     @Override
     protected int[] sort(int[] nums) {
+        final int reduce = 2;
+        int length = nums.length;
+        int step = nums.length;
+        while((step=step/reduce)!=1){
+            for (int i=step;i<length;i++){
+                int mid = nums[i];
+                for (int j=i-step;j>=0;j-=step){
+                    if (mid<nums[j]){
+                        nums[j+step]=nums[j];
+                    }else{
+                        nums[j+step]=mid;
+                        break;
+                    }
+                    if (j<step){
+                        nums[j]=mid;
+                        break;
+                    }
+                }
+            }
+        }
         return super.sort(nums);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100,23,1,23,3,5,2,2,5,6,8,1,5,10,22,434,1,3,4,5,8,2,0,3,1,6,4};
+        ShellSort shellSort = new ShellSort();
+        int[] ints = shellSort.toSort(nums);
+        for (int anInt : ints) {
+            System.out.print(" ");
+            System.out.print(anInt);
+        }
     }
 }
