34a29a7dd08f1d56e707fa7eac276a4e4d9332cf
damon <damon0425@outlook.com>
2020-09-10 15:15:05 +0800

    1.java8的一些语法尝试

---
+++ src/main/java/com/damon/java8/Apple.java
@@ -0,0 +1,30 @@
+package com.damon.java8;
+
+/**
+ * @ClassName Apple
+ * @Description TODO
+ * @Author Damon
+ * @Date 2020/8/20 下午1:42
+ * @Version 1.0.0
+ **/
+public class Apple {
+    private Integer weight;
+    private String  color;
+
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
